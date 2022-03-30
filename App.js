a16ef2bcc01049691d16ec5e60e17395d5e21b76
tulyainam <34723378+tulyainam@users.noreply.github.com>
2022-03-30 13:21:15 +0300

    appjs

--- App.js
+++ App.js
@@ -1,112 +1,54 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
+import React, { Component } from 'react';
+import { Text, StyleSheet, View } from 'react-native';
+import DrawerNavigator from './src/module/navigation/DrawerNavigator';
+// import firebase from 'firebase';
+import { Provider } from 'react-redux';
+import { applyMiddleware, createStore } from 'redux';
+import ReduxThunk from 'redux-thunk';
+import reducers from './src/reducers';
+import AsyncStorage from '@react-native-community/async-storage';
 
-import React from 'react';
-import type {Node} from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
+class App extends Component {
+  /* componentDidMount() {
+    firebase.initializeApp (
+      {
+        apiKey: "AIzaSyAUj0Rn8qvmvDW2BztcCWgbEFc7dM96Hd8",
+        authDomain: "adisyon-b56bd.firebaseapp.com",
+        projectId: "adisyon-b56bd",
+        storageBucket: "adisyon-b56bd.appspot.com",
+        messagingSenderId: "865007782483",
+        appId: "1:865007782483:web:f789cf85df2e5568f454a6",
+        measurementId: "G-4TWJ25MF4K"
+      }
+    )
+  } */
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+ /*  async getToken() {
+    var fcmToken = await AsyncStorage.getItem('fcmToken');
+    console.log('fcmToken:   ' + fcmToken);
+    if(!fcmToken) {
+      fcmToken = await firebase.messaging().getToken();
+      if (fcmToken) {
+        console.log("after fcmToken", fcmToken);
+        await AsyncStorage.setItem('fcmToken', fcmToken);
+      } else {
+        console.log("again requestPermission");
+      }
+    } 
+    console.log("before fcmToken", fcmToken);
+    return fcmToken;
+  } */
 
-const Section = ({children, title}): Node => {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-};
+  render() {
+    const store = createStore(reducers, {}, applyMiddleware(ReduxThunk));
+    return (
+      <Provider store={store}>
+        <DrawerNavigator/>
+      </Provider>
+    )
+  }
+}
 
-const App: () => Node = () => {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
-  return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.js</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
-  );
-};
-
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
+const styles = StyleSheet.create({})
 
 export default App;
